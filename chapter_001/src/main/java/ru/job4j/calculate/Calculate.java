b6014c072c7e3235133ea6d4aca35b08d0ce154f
Aleksey Vdovenko <a.a.vd0venk0@gmail.com>
2018-10-11 22:01:15 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+* 
+* @author Aleksey Vdovenko (a.a.vd0venk0@gmail.com)
+*/
 public class Calculate {
 
+	/**
+	* Main
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
