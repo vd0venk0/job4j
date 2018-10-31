96a319f10d4e732456a26cbddba8b157ffe6a342
Aleksey Vdovenko <a.a.vd0venk0@gmail.com>
2018-10-31 18:19:59 +0300

    4.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * @author Aleksey Vdovenko (a.a.vd0venk0@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class DummyBot {
+
+    /**
+     * Бот отвечает на вопросы.
+     * @param question Вопрос.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
