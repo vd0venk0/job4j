dcbe35625c3d563a6c738129f6afa86c0e568cff
Aleksey Vdovenko <a.a.vd0venk0@gmail.com>
2019-03-13 01:20:16 +0300

    002.4.4. Используя шаблон проектирования - стратегию [#786] v2

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertThat;
  */
 public class PaintTest {
     /**
-     * Test for Paint.
+     * Test for Paint - DrawSquare.
      */
     @Test
     public void whenDrawSquare() {
@@ -40,6 +40,31 @@ public class PaintTest {
                 )
         );
         // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+
+    /**
+     * Test for Paint - DrawTriangle.
+     */
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                        .append("    *\n")
+                        .append("   **\n")
+                        .append("  * *\n")
+                        .append(" *  *\n")
+                        .append("*****\n")
+                        .append(System.lineSeparator())
+                        .toString()
+                )
+        );
         System.setOut(stdout);
     }
 }
