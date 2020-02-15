6a92a3d5f2860936097d0c3c56f4faf40ae225a4
Aleksey Vdovenko <a.a.vd0venk0@gmail.com>
2020-02-15 22:13:29 +0300

    Создание объекта [#165452]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+/**
+ * Cat.
+ *
+ * @author Aleksey Vdovenko (a.a.vd0venk0@gmail.com)
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+/**
+ * Dog.
+ *
+ * @author Aleksey Vdovenko (a.a.vd0venk0@gmail.com)
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
