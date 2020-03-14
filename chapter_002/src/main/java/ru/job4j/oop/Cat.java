0f88e383269acc661d7f90f799cb310a1440e5ed
Aleksey Vdovenko <a.a.vd0venk0@gmail.com>
2020-03-14 05:18:38 +0300

    1.4. Вызов метод с возвращаемым типом. [#167387]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -5,8 +5,15 @@ package ru.job4j.oop;
  * @author Aleksey Vdovenko (a.a.vd0venk0@gmail.com)
  */
 public class Cat {
+
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
     }
 }
---
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+/**
+ * DummyDic.
+ * @author Aleksey Vdovenko (a.a.vd0venk0@gmail.com).
+ *
+ * 1. Создайте класс ru.job4j.oop.DummyDic.
+ * 2. Создайте в нем метод public String engToRus(String eng).
+ * 3. Метод должен вернуть строчку "Неизвестное слово. " + eng.
+ * 4. Залейте код в репозиторий.
+ * 5. Переведите ответственного на Петра Арсентьева.
+ */
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        return ("Неизвестное слово." + eng);
+    }
+
+}
