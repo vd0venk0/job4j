97a4792f85f14d4105b14d5551967efbbb050ebd
Aleksey Vdovenko <a.a.vd0venk0@gmail.com>
2021-01-22 15:23:40 +0300

    0.1. java.lang.NullPointerException [#219364]

---
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+
+
+    public static int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("агрумент должен быть положительным");
+        }
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+    public static void main(String[] args) {
+        System.out.println(calc(10));
+        System.out.println(calc(-1));
+    }
+}
